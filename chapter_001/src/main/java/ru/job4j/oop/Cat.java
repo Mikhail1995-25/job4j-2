2d9c76fec39709af8266c6e1093d0ee6faa6d2e3
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-03-08 21:19:09 +0300

    1. Создание обьекта.[#231134]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+/**
+ * Cat
+ * @author Mikhail Pushkarev
+ * @since 08.03.2020
+ * @version 0.1
+ */
+public class Cat {
+    /**
+     * main
+     * @param args - Выводит на консоль результат.
+     */
+    public static void main(String[] args) {
+        Cat Peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+/**
+ * Dog
+ * @author Mikhail Pushkarev
+ * @since 08.03.2020
+ * @version 0.2
+ */
+public class Dog {
+    /**
+     * Main
+     * @param args - Выводит результат на консоль.
+     */
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
