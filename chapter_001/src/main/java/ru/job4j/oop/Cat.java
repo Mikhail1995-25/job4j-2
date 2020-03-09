6890e67acfe27b4275b6230a12b34b4100433044
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-03-09 19:35:46 +0300

    Поле объекта [#231105]

--- chapter_001/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -7,12 +7,48 @@ package ru.job4j.oop;
  * @version 0.1
  */
 public class Cat {
+    private String food;
+    private String name;
+
+    /**
+     * Method show - Метод выводит содержимое поля.
+     */
+    public void show() {
+        System.out.println(this.food);
+        System.out.println(this.name);
+    }
+
+    /**
+     * Method eat
+     * @param meat - Параметр принимает строку и записывает в поле.
+     */
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    /**
+     * Method givNick
+     * @param nick - Метод так же принимает строку, далее записывает в поле.
+     */
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
     /**
      * main
      * @param args - Выводит на консоль результат.
      */
     public static void main(String[] args) {
-        Cat Peppy = new Cat();
-        Cat sparky = new Cat();
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("black");
+        black.eat("fish");
+        black.show();
+
+
     }
 }
