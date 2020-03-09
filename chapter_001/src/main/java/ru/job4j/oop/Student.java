a72c04f2c84c15bfbfe9e67c070feb9741fc2428
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-03-09 17:12:25 +0300

    2. Вызов метода объекта [#231106].

---
+++ chapter_001/src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,39 @@
+package ru.job4j.oop;
+
+/**
+ * Student
+ * @author Mikhail Pushkarev
+ * @since 09.03.2020
+ * @version 0.3
+ */
+public class Student {
+    /**
+     * Method music - Студент Петя играет на баяне.
+     */
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    /**
+     * Method song - Петя поет песню.
+     */
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+
+    /**
+     * Main
+     * @param args - Вывести на конслоь результат.
+     */
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.song();
+        petya.song();
+        petya.song();
+
+
+    }
+}
