7e769832120b0186b139195cd93f42abd2e9f13e
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-03-15 21:36:55 +0300

    4. Расстояние между точками в трехмерном пространстве.[#231102]

--- chapter_002/src/main/java/ru/job4j/condition/Point.java
+++ chapter_002/src/main/java/ru/job4j/condition/Point.java
@@ -1,7 +1,6 @@
 package ru.job4j.condition;
 
-import static java.lang.Math.sqrt;
-import static java.lang.Math.pow;
+import static java.lang.Math.*;
 
 /**
  * Point
@@ -18,6 +17,7 @@ public class Point {
      * Поле объекта доступно определенному объекту.
      */
     private int y;
+    private int z;
 
     /**
      * Konstructor Point
@@ -28,6 +28,11 @@ public class Point {
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
+    }
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     /**
@@ -40,13 +45,22 @@ public class Point {
     }
 
     /**
+     * Method distance
+     * @param that - Принимает значение объекта для вычесления квадратного корня.
+     * @return - возвращает расстояние между точками координат.
+     */
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+
+    /**
      * Main
      * @param args - Выводит результат на консоль.
      */
     public static void main(String[] args) {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
-        double dist = a.distance(b);
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 0, 2);
+        double dist = a.distance3d(b);
         System.out.println(dist);
     }
 }
