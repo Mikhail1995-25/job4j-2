02dd70a9632c2eb1a3a4300414e18b3da20a48d9
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-04-14 15:09:09 +0300

    0.5. Иерархия исключений и множественный catch.[#231094]

---
+++ chapter_002/src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,49 @@
+package ru.job4j.ex;
+
+/**
+ * @author Mikhail Pushkarev
+ * @version 14.04.2020
+ * @since 0.1
+ */
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User us = null;
+        for (int index = 0; index < users.length; index++) {
+            User one = users[index];
+            if (one.getUsername().equals(login)) {
+               us = one;
+            }
+        }
+        if (us == null) {
+            throw new UserNotFoundException("Not found");
+        }
+        return us;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user.isValid() == false) {
+            throw new UserInvalidException("Not validate");
+        }
+        if (user.getUsername().length() < 3) {
+            throw new UserInvalidException("Not found");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Mikhail Pushkarev", true)
+        };
+        try {
+            User user = findUser(users, "Mikhail Pushkarev");
+           if (validate(user)) {
+               System.out.println("This user has an access");
+           }
+        } catch (UserInvalidException ex) {
+            ex.printStackTrace();
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
