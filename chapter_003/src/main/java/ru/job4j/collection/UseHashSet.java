a6ea27a8c36b83f000dd38b92a1aadb48ccaa8a4
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-04-26 14:51:51 +0300

    0. Set, HashSet, Iterator. [#231054]

---
+++ chapter_003/src/main/java/ru/job4j/collection/UseHashSet.java
@@ -0,0 +1,28 @@
+package ru.job4j.collection;
+
+import java.io.PrintStream;
+import java.util.HashSet;
+
+public class UseHashSet {
+    public static void main(String[] args) {
+        HashSet<String> names = new HashSet<String>();
+        names.add("Mikhail");
+        names.add("Vasiliy");
+        names.add("Vasiliy");
+        names.add("Vlad");
+        for (String name : names) {
+            System.out.println(name);
+        }
+        System.out.println();
+        HashSet<Integer> number = new HashSet<>();
+        number.add(1);
+        number.add(1);
+        number.add(2);
+        number.add(2);
+        number.add(3);
+        number.add(3);
+        for (Integer num : number) {
+            System.out.print(num);
+        }
+    }
+}
