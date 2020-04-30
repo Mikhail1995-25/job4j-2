b12a99e967eac16d3f4cc31cec60cfda642ccae7
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-04-30 12:52:08 +0300

    0. Сортировка[#231036]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,37 +1,31 @@
 package ru.job4j.tracker;
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
-
-import java.awt.desktop.SystemEventListener;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.lang.module.FindException;
-import java.util.StringJoiner;
 public class StartUITest {
-
     @Test
     public void whenAddItem() {
         String[] answers = {"Fix PC"};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
         new CreateAction().execute(input, tracker);
-        Item created = tracker.findAll()[0];
+        Item created = tracker.findAll().get(0);
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
-
     @Test
     public void whenAdd2Item() {
         String[] answers = {"Test method"};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
         new CreateAction().execute(input, tracker);
-        Item created = tracker.findAll()[0];
+        Item created = tracker.findAll().get(0);
         Item expected = new Item("Test method");
         assertThat(created.getName(), is(expected.getName()));
     }
-
     @Test
     public void whenReplaceItem() {
         Tracker tracker = new Tracker();
@@ -45,16 +39,15 @@ public class StartUITest {
         Item replaced = tracker.findById(item.getId());
         assertThat(replaced.getName(), is("replaced item"));
     }
-
     @Test
     public void whenDeleteItem() {
         Tracker tracker = new Tracker();
-       Item item = new Item("replaced item");
-       tracker.add(item);
-       String[] answers = {item.getId()};
-       new DeleteAction().execute(new StubInput(answers), tracker);
-       Item deleteItem = tracker.findById(item.getId());
-       assertNull(deleteItem);
+        Item item = new Item("replaced item");
+        tracker.add(item);
+        String[] answers = {item.getId()};
+        new DeleteAction().execute(new StubInput(answers), tracker);
+        Item deleteItem = tracker.findById(item.getId());
+        assertNull(deleteItem);
     }
 
     @Test
@@ -63,62 +56,8 @@ public class StartUITest {
                 new String[] {"0"}
         );
         StubAction action = new StubAction();
-        new StartUI().init(input, new Tracker(), new UserAction[] {action});
+        new StartUI().init(input, new Tracker(), Arrays.asList(new UserAction[]{action}));
         assertThat(action.isCall(), is(true));
-    }
-
-    @Test
-    public void whenPrtMenu() {
-        ByteArrayOutputStream bao = new ByteArrayOutputStream();
-        PrintStream ps = System.out;
-        System.setOut(new PrintStream(bao));
-        StubInput input = new StubInput(
-                new String[] {"0"}
-        );
-        StubAction action = new StubAction();
-        new StartUI().init(input, new Tracker(), new UserAction[] { action });
-        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
-                .add("Menu.")
-                .add("0. Stub action")
-                .toString();
-        assertThat(new String(bao.toByteArray()), is(expect));
-        System.setOut(ps);
 
     }
-
-    @Test
-    public void whenCheckOutPut() {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PrintStream def = System.out;
-        System.setOut(new PrintStream(out));
-        Tracker tracker = new Tracker();
-        Item item = new Item("fix bug");
-        tracker.add(item);
-        FindAllAction act = new FindAllAction();
-        act.execute(new StubInput(new String[] {}), tracker);
-        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
-                .add(item.toString())
-                .toString();
-        assertThat(new String(out.toByteArray()), is(expect));
-        System.setOut(def);
-
-    }
-
-    @Test
-    public void whenCheckOut() {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PrintStream def = System.out;
-        System.setOut(new PrintStream(out));
-        Tracker tracker = new Tracker();
-        Item item = new Item("check method");
-        tracker.add(item);
-        FindByNameAction name = new FindByNameAction();
-        name.execute(new StubInput(new String[] {"check method"}), tracker);
-        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
-                .add(item.toString())
-                .toString();
-        assertThat(new String(out.toByteArray()), is(expect));
-        System.setOut(def);
-    }
-
 }
---
+++ chapter_003/src/main/java/ru/job4j/collection/JobSorters.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class JobSorters {
+    public static void main(String[] args) {
+        List<Job> list =  Arrays.asList(
+                new Job("Fix bugs", 4),
+                new Job("Impl task", 2),
+                new Job("Reboot server", 1)
+        );
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+        Collections.sort(list, new SortByNameJob());
+        System.out.println(list);
+    }
+}
