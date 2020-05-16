e063c665348ae9be06b4bc623770bc9844063bd8
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-05-16 19:05:35 +0300

    4. Преобразование List в Map.[#230907]

--- chapter_003/src/main/java/ru/job4j/collection/School.java
+++ chapter_003/src/main/java/ru/job4j/collection/School.java
@@ -1,12 +1,21 @@
 package ru.job4j.collection;
-
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
 
     public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        return students.stream().filter(predict).collect(Collectors.toList());
+        return students.stream().filter(predict).distinct().collect(Collectors.toList());
+    }
+
+    public static Map<Student, String> collector(List<Student> students) {
+        return students.stream().distinct().collect(
+                Collectors.toMap(
+                        student -> student,
+                        Student::getSurname
+                )
+        );
     }
 }
--- chapter_003/src/test/java/ru/job4j/collection/SchoolTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/SchoolTest.java
@@ -1,7 +1,10 @@
 package ru.job4j.collection;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -42,5 +45,36 @@ public class SchoolTest {
         List<Student> third = School.collect(list, student -> student.getScore() >= 0 && student.getScore() < 50);
         List<Student> list1 = List.of(new Student(15, "Smirnov"), new Student(30, "Pupkin"));
         assertThat(third.toString(), is(list1.toString()));
+    }
+
+    @Test
+    public void whenListToMap() {
+      List<Student> list = Arrays.asList(
+              new Student(80, "Petrov"),
+              new Student(30, "Ivanov"),
+              new Student(90, "Sokolov"),
+              new Student(50, "Vasichkin"),
+              new Student(20, "Oleshin"),
+              new Student(60, "Kozlov"),
+              new Student(15, "Smirnov"),
+              new Student(30, "Pupkin"),
+              new Student(100, "Kasin")
+      );
+      Map<Student, String> map = School.collector(list);
+      Map<Student, String> expected = new HashMap<>();
+
+      expected.put(list.get(0), list.get(0).getSurname());
+        expected.put(list.get(1), list.get(1).getSurname());
+        expected.put(list.get(2), list.get(2).getSurname());
+        expected.put(list.get(3), list.get(3).getSurname());
+        expected.put(list.get(4), list.get(4).getSurname());
+        expected.put(list.get(5), list.get(5).getSurname());
+        expected.put(list.get(6), list.get(6).getSurname());
+        expected.put(list.get(7), list.get(7).getSurname());
+        expected.put(list.get(8), list.get(8).getSurname());
+
+
+      assertThat(map, is(expected));
+
     }
 }
